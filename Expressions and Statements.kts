c22ddfae58ed01b52e60ec0a37c0900f629703f4
bagusbima <53173441+bagusbima@users.noreply.github.com>
2020-09-20 16:01:19 +0700

    Add files via upload

---
+++ Expressions and Statements.kts
@@ -0,0 +1,8 @@
+fun main() {
+val value1 = 23
+val value2 = 32
+
+sum(value1, value2)
+}
+
+fun sum(value1: Int, value2: Int) = value1 + value2
---
+++ While and Do While.kts
@@ -0,0 +1,7 @@
+fun main() {
+    var counter = 1
+    while (counter <= 7){
+        println("GoesBeem")
+        counter++
+    }
+}
